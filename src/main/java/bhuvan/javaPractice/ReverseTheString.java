0f4d07957d0f7ea2f3e1ec25a4ef68e09995c70a
bhuwaneshuwaran.vasu <bhuwaneshuwaran.vasu@host.docker.internal>
2023-10-07 09:17:20 +0530

    Initial Commit

---
+++ src/main/java/bhuvan/javaPractice/ReverseTheString.java
@@ -0,0 +1,18 @@
+package bhuvan.javaPractice;
+
+public class ReverseTheString {
+
+	public static void main(String[] args) {
+
+		String text = "Bhuvan", reversedString = "";
+		char ch;
+		
+		for (int i =0; i < text.length(); i++) {
+			ch = text.charAt(i);
+			reversedString = ch+reversedString;
+		}
+		
+		System.out.println(reversedString);
+	}
+
+}
